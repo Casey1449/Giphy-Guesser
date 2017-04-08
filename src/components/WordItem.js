bc370c87f0af40919dd79aa67ff3ac512cdac077
Casey Metz <caseymetz@gmail.com>
2017-04-08 00:20:08 -0600

    px sizes

--- src/components/WordItem.js
+++ src/components/WordItem.js
@@ -3,16 +3,25 @@ import '../styles/ListItems.css';
 import { SortableElement } from 'react-sortable-hoc';
 
 const WordItem = SortableElement((props) =>
-<svg version="1.1" preserveAspectRatio="none" x='0' y='0' width='100px' height='60px' viewBox="0 0 110 56" fill='white'><polygon points="0,0 0,50 90,50 110,25 90,0 "/></svg>
-  );
 
+<li className='list-item word-item'>
+<div className='word-arrow'>
+<p>{props.word}</p>
+</div>
+</li>
+);
 export default WordItem;
 
 
 
 
-// <li className='list-item word-item'>
-// <div className='word-arrow'>
-// <p>{props.word}</p>
+
+
+// <div>
+// <svg className='white-arrow' version="1.1" preserveAspectRatio="none" >
+// <g>
+// <polygon points="0,0 0,50 90,50 110,25 90,0 "/>
+// <text x="50%" y="50%" textAnchor="middle" fill='black'>Stuff in here</text>
+// </g>
+// </svg>
 // </div>
-// </li>
