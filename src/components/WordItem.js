dbf0204cf0cad1e925c1282185b6f90ca4d65d9e
Casey Metz <caseymetz@gmail.com>
2017-04-08 15:57:45 -0600

    background-size percents

--- src/components/WordItem.js
+++ src/components/WordItem.js
@@ -4,24 +4,12 @@ import { SortableElement } from 'react-sortable-hoc';
 
 const WordItem = SortableElement((props) =>
 
-<li className='list-item word-item'>
-<div className='word-arrow'>
-<p>{props.word}</p>
-</div>
-</li>
+  <li className='list-item word-item'>
+    <div className='word-arrow'>
+      <p>{props.word}</p>
+    </div>
+  </li>
+
 );
+
 export default WordItem;
-
-
-
-
-
-
-// <div>
-// <svg className='white-arrow' version="1.1" preserveAspectRatio="none" >
-// <g>
-// <polygon points="0,0 0,50 90,50 110,25 90,0 "/>
-// <text x="50%" y="50%" textAnchor="middle" fill='black'>Stuff in here</text>
-// </g>
-// </svg>
-// </div>
