bf8ba55ee3a737bd10f230658488c0b840667a3b
Casey Metz <caseymetz@gmail.com>
2017-04-07 22:59:22 -0600

    trying in-line svg

--- src/components/WordItem.js
+++ src/components/WordItem.js
@@ -3,12 +3,16 @@ import '../styles/ListItems.css';
 import { SortableElement } from 'react-sortable-hoc';
 
 const WordItem = SortableElement((props) =>
-  <li className='list-item word-item'>
-    <div className='word-arrow'>
-      <p>{props.word}</p>
-    </div>
-  </li>
-
+<svg version="1.1" preserveAspectRatio="none" x='0' y='0' width='100px' height='60px' viewBox="0 0 110 56" fill='white'><polygon points="0,0 0,50 90,50 110,25 90,0 "/></svg>
   );
 
 export default WordItem;
+
+
+
+
+// <li className='list-item word-item'>
+// <div className='word-arrow'>
+// <p>{props.word}</p>
+// </div>
+// </li>
