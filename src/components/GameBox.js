6fda96d22421219219af5abfa0c99cc5f0e803be
Casey Metz <caseymetz@gmail.com>
2017-04-09 15:38:05 -0600

    install redux, react-redux

--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -2,8 +2,12 @@ import React from 'react';
 import WordList from './WordList';
 import GifList from './GifList';
 import '../styles/GameBox.css';
+import randomWords from 'random-words';
 
 export default () => {
+
+  console.log(randomWords(4))
+
   return (
     <div className='game-box'>
       <WordList axis={'xy'} />
--- src/index.js
+++ src/index.js
@@ -7,3 +7,5 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
