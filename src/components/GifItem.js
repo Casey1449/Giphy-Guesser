2a56fefbc321b0ebb5355dac370817932a6203f4
Casey Metz <caseymetz@gmail.com>
2017-04-11 15:51:51 -0600

    -fix safari-chrome flexbox bugs

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -3,13 +3,32 @@ import '../styles/ListItems.css';
 import Spinner from './Spinner';
 
 export default ({ value }) => {
+
+  if(value.desktop){
+
+    const gifStyle = {
+      background: `url(${value.desktop.url}) no-repeat`,
+      backgroundPosition: 'center',
+      backgroundSize: 'contain'
+    };
+
+    return (
+      <li className='list-item gif-item'
+          style = {gifStyle}>
+        <p>Test Content</p>
+      </li>
+    );
+  }
+
   return (
     <li className='list-item gif-item'>
-      { !value.desktop ?
-          <div style={{color: 'black'}}>{value}</div>
-        :
-          <img className='gif' src={value.desktop.url} alt='gif'/>
-      }
+      <Spinner />
     </li>
-  )
+  );
 };
+//
+// { !value.desktop ?
+//   <div style={{color: 'black'}}>{value}</div>
+//   :
+//   <img className='gif' src={value.desktop.url} alt='gif'/>
+// }
