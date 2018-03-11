507ea1e9cfa5aa4347e2bde4b908f198576a1714
Casey Metz <caseymetz@Barberhops-MacBook-Pro.local>
2018-03-10 18:39:01 -0700

    fix app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import "es6-promise";
+
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import rootReducer from "./reducers/index.js";
+import "./index.scss";
+
+const middleware = applyMiddleware(thunkMiddleware);
+
+let store = createStore(
+  rootReducer,
+  compose(
+    middleware,
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  )
+);
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
 });
